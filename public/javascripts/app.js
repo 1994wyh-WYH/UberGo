4164ab34a7afe9ca7fe4989106b906a9c12e07a2
Yue Yin <yyin@seas.upenn.edu>
2017-11-24 16:43:34 -0500

    init project

---
+++ public/javascripts/app.js
@@ -0,0 +1,38 @@
+var app = angular.module('angularjsNodejsTutorial',[]);
+app.controller('myController', function($scope, $http) {
+        $scope.message="";
+        $scope.Submit = function() {
+        var email = !!$scope.email ? $scope.email : undefined;
+        var request = $http.get('/data/'+email);
+        request.success(function(data) {
+            $scope.data = data;
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+    }; 
+});
+
+app.controller('insertController', function($scope, $http) {
+        $scope.message="";
+        $scope.Insert = function() {
+        var data = {}
+        data.login = !!$scope.login ? $scope.login : "";
+        data.name = !!$scope.name ? $scope.name : "";
+        data.sex = !!$scope.sex ? $scope.sex : "";
+        data.RelationshipStatus = !!$scope.RelationshipStatus ? $scope.RelationshipStatus : "";
+        data.Birthyear = !!$scope.Birthyear ? $scope.Birthyear : "";
+        console.log(data);
+        var request = $http.post('/insert/', data);
+        request.success(function(data) {
+            document.getElementById('insert-result-msg').textContent ='Insertion succeeded';
+        });
+        request.error(function(data){
+            document.getElementById('insert-result-msg').textContent = 'Insertion failed';
+        });
+    };
+});
+
+// To implement "Insert a new record", you need to:
+// - Create a new controller here
+// - Create a corresponding route handler in routes/index.js
---
+++ routes/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+// Connect string to MySQL
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'fling.seas.upenn.edu',
+  user     : 'chuqihe',
+  password : 'He19940408$',
+  database : 'chuqihe'
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
+});
+
+router.get('/reference', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'reference.html'));
+});
+
+router.get('/insert', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'insert.html'));
+});
+
+router.post('/insert', function(req, res, next) {
+  var query = 'INSERT INTO Person VALUES("'+req.body.login+'","'+req.body.name+'","'+req.body.sex+'","'+req.body.RelationshipStatus+'",'+req.body.Birthyear+');'
+  console.log(query);
+  connection.query(query, function(err, rows, fields) {
+    if (err) {
+      console.log(err);
+      res.status(500).send({ error: 'Something failed!' })
+    }
+    else {
+        res.send('Inserted');
+    }  
+    });
+});
+
+router.get('/data/:email', function(req,res) {
+  // use console.log() as print() in case you want to debug, example below:
+  // console.log("inside person email");
+  var query = 'SELECT Person.login, name, sex, relationshipStatus, birthyear, sum(case when Friends.login is null then 0 else 1 end) AS numFriends FROM Person LEFT JOIN Friends ON Person.login = Friends.login GROUP BY Person.login, Person.name, Person.sex, Person.relationshipStatus, Person.birthyear';
+  // you may change the query during implementation
+  var email = req.params.email;
+  if (email != 'undefined') query = query + ' having login ="' + email + '"' ;
+  console.log(query);
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+        res.json(rows);
+    }  
+    });
+});
+
+// ----Your implemention of route handler for "Insert a new record" should go here-----
+
+
+module.exports = router;
